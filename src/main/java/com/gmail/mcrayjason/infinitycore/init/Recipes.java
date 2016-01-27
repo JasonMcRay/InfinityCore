d2e5bd836399e9c8ea446ae54035754e7a077fd9
Jason McRay <mcrayjason@gmail.com>
2016-01-27 14:06:34 +0100

    Hardcoded Enderium Base recipe
    Because Minetweaker is not able to set EU/t.
    Recipe has been moved to 300EU/t - HV

--- src/main/java/com/gmail/mcrayjason/infinitycore/init/Recipes.java
+++ src/main/java/com/gmail/mcrayjason/infinitycore/init/Recipes.java
@@ -2,10 +2,12 @@ package com.gmail.mcrayjason.infinitycore.init;
 
 import cpw.mods.fml.common.registry.GameRegistry;
 import crazypants.enderio.EnderIO;
+import gregtech.api.enums.GT_Values;
 import gregtech.api.enums.Materials;
 import gregtech.api.enums.OrePrefixes;
 import gregtech.api.util.GT_OreDictUnificator;
 import net.minecraft.item.ItemStack;
+import net.minecraftforge.fluids.FluidRegistry;
 import net.minecraftforge.oredict.ShapedOreRecipe;
 import net.minecraftforge.oredict.ShapelessOreRecipe;
 
@@ -37,5 +39,9 @@ public class Recipes
         GameRegistry.addSmelting(new ItemStack(ModItems.itemCluster, 1, 13),(GT_OreDictUnificator.get(OrePrefixes.ingot, Materials.Uranium, 2L)), 0F);
         GameRegistry.addSmelting(new ItemStack(ModItems.itemCluster, 1, 14),(GT_OreDictUnificator.get(OrePrefixes.ingot, Materials.Zinc, 2L)), 0F);
         GameRegistry.addSmelting(new ItemStack(ModItems.itemCluster, 1, 15),(GT_OreDictUnificator.get(OrePrefixes.ingot, Materials.Tin, 4L)), 0F);
+
+        // GregTech Processing Recipes
+        GT_Values.RA.addChemicalRecipe(GT_OreDictUnificator.get(OrePrefixes.dust, Materials.Silver, 1L), GT_OreDictUnificator.get(OrePrefixes.dust, Materials.Platinum, 1L), Materials.Tin.getMolten(288), Materials.EnderiumBase.getMolten(576), null, 100, 300);
+
     }
 }

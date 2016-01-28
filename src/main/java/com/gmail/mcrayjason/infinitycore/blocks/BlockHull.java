d7e1a7ab02f4cdf515e1dbb99978d8722cd9c235
Jason McRay <mcrayjason@gmail.com>
2016-01-28 11:27:07 +0100

    Pneumatic Hulls facing player on placed

--- src/main/java/com/gmail/mcrayjason/infinitycore/blocks/BlockHull.java
+++ src/main/java/com/gmail/mcrayjason/infinitycore/blocks/BlockHull.java
@@ -5,7 +5,11 @@ import com.gmail.mcrayjason.infinitycore.creativetab.CreativeTabIFC;
 import net.minecraft.block.Block;
 import net.minecraft.block.material.Material;
 import net.minecraft.client.renderer.texture.IIconRegister;
+import net.minecraft.entity.EntityLivingBase;
+import net.minecraft.item.ItemStack;
 import net.minecraft.util.IIcon;
+import net.minecraft.util.MathHelper;
+import net.minecraft.world.World;
 import net.minecraftforge.common.util.ForgeDirection;
 
 public class BlockHull extends Block {
@@ -19,23 +23,29 @@ public class BlockHull extends Block {
         this.setHarvestLevel("pickaxe", 2);
     }
 
-    public IIcon topTexture;
+    public IIcon frontTexture;
     public IIcon defaultTexture;
 
     public void registerBlockIcons(IIconRegister regIcon)
     {
-        topTexture = regIcon.registerIcon(Reference.MODID + ":" + getUnlocalizedName().substring(5) + "_TOP");
+        frontTexture = regIcon.registerIcon(Reference.MODID + ":" + getUnlocalizedName().substring(5) + "_FRONT");
         defaultTexture = regIcon.registerIcon(Reference.MODID + ":" + getUnlocalizedName().substring(5));
     }
 
     public IIcon getIcon(int side, int meta)
     {
-        switch(ForgeDirection.getOrientation(side))
-        {
-            case UP:
-                return topTexture;
-            default:
-                return defaultTexture;
-        }
+        if (side == 1) return this.defaultTexture;
+        else if (side == 0) return this.defaultTexture;
+        else if (meta == 2 && side == 2) return this.frontTexture;
+        else if (meta == 3 && side == 5) return this.frontTexture;
+        else if (meta == 0 && side == 3) return this.frontTexture;
+        else if (meta == 1 && side == 4) return this.frontTexture;
+        else return this.defaultTexture;
+    }
+
+    public void onBlockPlacedBy(World par1World, int x, int y, int z, EntityLivingBase par5EntityLivingBase, ItemStack par6ItemStack)
+    {
+        int whichDirectionFacing = MathHelper.floor_double((double)(par5EntityLivingBase.rotationYaw * 4.0F / 360.0F) + 2.5D) & 3;
+        par1World.setBlockMetadataWithNotify(x, y, z, whichDirectionFacing, 2);
     }
 }

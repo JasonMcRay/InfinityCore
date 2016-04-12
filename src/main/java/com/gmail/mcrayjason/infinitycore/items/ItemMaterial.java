f95bc252f9d94fc90a77dcc605883584dae75d7a
JasonMcRay <mcrayjason@gmail.com>
2016-04-12 19:05:37 +0200

    Added Balanced Infused Seeds
    Closes #2

--- src/main/java/com/gmail/mcrayjason/infinitycore/items/ItemMaterial.java
+++ src/main/java/com/gmail/mcrayjason/infinitycore/items/ItemMaterial.java
@@ -11,7 +11,7 @@ import java.util.List;
 
 public class ItemMaterial extends ItemIFC
 {
-    int itemCount = 17;
+    int itemCount = 18;
 
     public IIcon[] icons = new IIcon[itemCount];
     public ItemMaterial(String name) {
@@ -38,6 +38,7 @@ public class ItemMaterial extends ItemIFC
         this.icons[14] = regIcon.registerIcon(Reference.MODID + ":" + "materials/" + "rodSandstone");
         this.icons[15] = regIcon.registerIcon(Reference.MODID + ":" + "materials/" + "rodHardenedClay");
         this.icons[16] = regIcon.registerIcon(Reference.MODID + ":" + "materials/" + "foodSalt");
+        this.icons[17] = regIcon.registerIcon(Reference.MODID + ":" + "materials/" + "seedsBalancedInfused");
     }
 
     @Override
